c53415ef27ce383be9439682ff4e06079f1e5030
许开伟 <wanghao@xukaiweiMacmini.local>
2014-11-17 09:54:34 +0800

    Initial Commit

---
+++ DecorationDesign/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DecorationDesign
+//
+//  Created by 许开伟 on 14/11/17.
+//  Copyright (c) 2014年 许开伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DecorationDesign/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DecorationDesign
+//
+//  Created by 许开伟 on 14/11/17.
+//  Copyright (c) 2014年 许开伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
