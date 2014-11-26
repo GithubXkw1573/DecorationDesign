2ebdbf76bcdf6c594799755f5fedb9a9f083735a
许开伟 <wanghao@xukaiweiMacmini.local>
2014-11-26 11:41:12 +0800

    提交Deletegate文件

--- DecorationDesign/DDAppDelegate.h
+++ DecorationDesign/DDAppDelegate.h
@@ -7,9 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "HomeViewController.h"
+#import "TabbarView.h"
+#import "CoverViewController.h"
+#import "WodeViewController.h"
+#import "DistributeController.h"
+#import "BookingController.h"
 
-@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate,UITabBarControllerDelegate,TabbarViewDelegate>
+{
+    UITabBarController *TabBarController;
+    HomeViewController *homeCtrl;
+    DistributeController *distributeCtrl;
+    BookingController *bookingCtrl;
+    WodeViewController *wodeCtrl;
+    UINavigationController *homeNavi;
+    UINavigationController *tab2Navi;
+    UINavigationController *tab3Navi;
+    UINavigationController *tab4Navi;
+    TabbarView *barView1;
+    TabbarView *barView2;
+    TabbarView *barView3;
+    TabbarView *barView4;
+}
 
 @property (strong, nonatomic) UIWindow *window;
+@property(nonatomic,retain)UITabBarController *TabBarController;
+@property (nonatomic,retain) HomeViewController *homeCtrl;
+@property (nonatomic,retain) DistributeController *distributeCtrl;
+@property (nonatomic,retain) BookingController *bookingCtrl;
+@property (nonatomic,retain) WodeViewController *wodeCtrl;
+@property (nonatomic,retain) UINavigationController *homeNavi;
+@property (nonatomic,retain) UINavigationController *tab2Navi;
+@property (nonatomic,retain) UINavigationController *tab3Navi;
+@property (nonatomic,retain) UINavigationController *tab4Navi;
 
 @end
--- DecorationDesign/DDAppDelegate.m
+++ DecorationDesign/DDAppDelegate.m
@@ -9,14 +9,136 @@
 #import "DDAppDelegate.h"
 
 @implementation DDAppDelegate
+@synthesize homeCtrl,distributeCtrl,bookingCtrl,wodeCtrl;
+@synthesize homeNavi,tab2Navi,tab3Navi,tab4Navi;
+@synthesize TabBarController;
+
+- (void)dealloc
+{
+    [homeNavi release];
+    [tab2Navi release];
+    [tab3Navi release];
+    [tab4Navi release];
+    [barView1 release];
+    [barView2 release];
+    [barView3 release];
+    [barView4 release];
+    [homeCtrl release];
+    [distributeCtrl release];
+    [bookingCtrl release];
+    [TabBarController release];
+    [_window release];
+    
+    [super dealloc];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];  //状态栏字体改为白色
+    
+    TabBarController=[[UITabBarController alloc]init];
+    TabBarController.delegate=self;
+    
+    self.homeCtrl = [[[HomeViewController alloc] init] autorelease];
+    self.distributeCtrl = [[[DistributeController alloc] init] autorelease];
+    self.bookingCtrl = [[[BookingController alloc] init] autorelease];
+    self.wodeCtrl = [[[WodeViewController alloc] init] autorelease];
+    UINavigationController *navi1=[[UINavigationController alloc]initWithRootViewController:self.homeCtrl];
+    UINavigationController *navi2=[[UINavigationController alloc]initWithRootViewController:self.distributeCtrl];
+    UINavigationController *navi3=[[UINavigationController alloc]initWithRootViewController:self.bookingCtrl];
+    UINavigationController *navi4=[[UINavigationController alloc]initWithRootViewController:self.wodeCtrl];
+    self.homeNavi = navi1;
+    self.tab2Navi = navi2;
+    self.tab3Navi = navi3;
+    self.tab4Navi = navi4;
+    NSArray *Controllers=[[NSArray alloc]initWithObjects:navi1,navi2,navi3,navi4, nil];
+    TabBarController.viewControllers=Controllers;
+    [navi1 release];
+    [navi2 release];
+    [navi3 release];
+    [navi4 release];
+    [Controllers release];
+    
+    UIImageView *backimage=[[UIImageView alloc] init];
+    backimage.backgroundColor=[UIColor whiteColor];
+    backimage.frame=CGRectMake( 0, 0, applicationwidth, 49);
+    [[[self TabBarController] tabBar] insertSubview:backimage atIndex:7];
+    [backimage release];
+    
+    barView1=[[TabbarView alloc] initWithFrame:CGRectMake(0, 0, 80*widthRate, 49)];
+    barView1.tag=1;
+    barView1.delegate=self;
+    [barView1 setselect:YES];
+    [[[self TabBarController] tabBar] insertSubview:barView1 atIndex:9];
+    
+    barView2=[[TabbarView alloc] initWithFrame:CGRectMake(80*widthRate, 0, 80*widthRate, 49)];
+    barView2.tag=2;
+    barView2.delegate=self;
+    [barView2 setselect:NO];
+    [[[self TabBarController] tabBar] insertSubview:barView2 atIndex:9];
+    
+    barView3=[[TabbarView alloc] initWithFrame:CGRectMake(160*widthRate, 0, 80*widthRate, 49)];
+    barView3.tag=3;
+    barView3.delegate=self;
+    [barView3 setselect:NO];
+    [[[self TabBarController] tabBar] insertSubview:barView3 atIndex:9];
+    
+    barView4=[[TabbarView alloc] initWithFrame:CGRectMake(240*widthRate, 0, 80*widthRate, 49)];
+    barView4.tag=4;
+    barView4.delegate=self;
+    [barView4 setselect:NO];
+    [[[self TabBarController] tabBar] insertSubview:barView4 atIndex:9];
+    
+    //引导动画
+    CoverViewController *covercon=[[CoverViewController alloc]init];
+    [self.window addSubview:covercon.view];
+    [covercon release];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+-(void)TabbarViewDelegateBtnPressed:(int)tag
+{
+    [self.TabBarController setSelectedIndex:(tag-1)];
+    
+    [barView1 setselect:NO];
+    [barView2 setselect:NO];
+    [barView3 setselect:NO];
+    [barView4 setselect:NO];
+    
+    switch (tag) {
+        case 1:
+        {
+            [barView1 setselect:YES];
+        }
+            break;
+        case 2:
+        {
+            [barView2 setselect:YES];
+        }
+            break;
+        case 3:
+        {
+            [barView3 setselect:YES];
+        }
+            break;
+        case 4:
+        {
+            [barView4 setselect:YES];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
+{
+    return NO;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
