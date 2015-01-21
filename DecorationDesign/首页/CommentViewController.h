8de9e7cb1d5a7b176aed0b5c22cf57131f2e74bd
许开伟 <wanghao@xukaiweiMacmini.local>
2015-01-21 15:35:39 +0800

    version 1-21  项目整理

---
+++ DecorationDesign/首页/CommentViewController.h
@@ -0,0 +1,39 @@
+//
+//  CommentViewController.h
+//  DecorationDesign
+//
+//  Created by 许开伟 on 14/12/16.
+//  Copyright (c) 2014年 许开伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "common.h"
+#import "CommentCell.h"
+#import "ShareView.h"
+
+@interface CommentViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
+{
+    UITableView *m_tableView;
+    MBProgressHUD *MBProgress;
+    NSArray *m_array;
+    NSString *worksId;
+    NSString *worksType;
+    NSString *designerId;
+    NSString *designerName;
+    NSArray *m_jsonArr;
+    NSMutableArray *n_jsonArr;
+    NSString *commentNums;
+    UITextField *commentField;
+    BOOL isCommented;
+    UILabel *support1;
+    UILabel *support2;
+    UILabel *support3;
+}
+@property (nonatomic,retain) NSArray *m_array;
+@property (nonatomic,retain) NSArray *m_jsonArr;
+@property (nonatomic,retain) NSMutableArray *n_jsonArr;
+@property (nonatomic,retain) NSString *worksId;
+@property (nonatomic,retain) NSString *worksType;
+@property (nonatomic,retain) NSString *designerId;
+@property (nonatomic,retain) NSString *designerName;
+@end
